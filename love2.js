c4c64f8189734fb75f36dec0b967c991ac60cb7f
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2021-09-02 10:31:48 +0530

    Update love2.js

--- love2.js
+++ love2.js
@@ -1,4 +1,4 @@
 
 var a = prompt("Enter Your Name");
-alert("new updates launched . Injoy unlimited sms with 5 api,  3 api not working properly fixed v1.1.2."); 
+alert("new updates launched . Injoy unlimited sms with 5 api,  3 api not working properly fixed ,  Privacy Policy , Terms and Conditions index adding ,v1.1.2"); 
 
