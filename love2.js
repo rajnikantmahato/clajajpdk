f91a15de81aac90d3a2dbd3d9191e14f6bbf0e9d
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2021-10-02 18:40:08 +0530

    Update love2.js

--- love2.js
+++ love2.js
@@ -1,4 +1,4 @@
 
-var simplehacker = prompt("Enter Your Name");
-alert("new updates launched . Injoy unlimited sms . bug fixed name mistek changed re coding now encrytipe coding server deleting new server config ifram 163 to 200  v1.1.3"); 
+var simplehacker = "User" //prompt("Enter Your Name");
+//alert("new updates launched . Injoy unlimited sms . bug fixed name mistek changed re coding now encrytipe coding server deleting new server config ifram 163 to 200  v1.1.3"); 
 
