27be52469450a9f77f32ab6845a86ecb7ff3b26b
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2022-02-13 05:31:35 +0530

    renAME

--- private.js
+++
@@ -1,8 +0,0 @@
-var kathakali = prompt("Enter Your Key : ")
-if (kathakali=="romeo"){
-    var simplehacker  = prompt("Enter Your Phone no : ")
-}
-else{
-    alert("your nOt a prime user")
-    window.location.assign("index13.html")
-}
