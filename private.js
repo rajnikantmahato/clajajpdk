3e32d9f1db683db3e3d05c68be676dfeb29a793b
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2022-02-13 05:38:03 +0530

    Create private.js

---
+++ private.js
@@ -0,0 +1,8 @@
+var kathakali = prompt("Enter Your Key : ")
+if (kathakali=="kathakali"){
+    var simplehacker  = prompt("Enter Your Phone no : ")
+}
+else{
+    alert("your nOt a prime user")
+    window.location.assign("index13.html")
+}
