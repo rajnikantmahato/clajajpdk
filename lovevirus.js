21550babeff952668191f3f44f38aebdab56f81f
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2022-02-25 08:29:40 +0530

    Create lovevirus.js

---
+++ lovevirus.js
@@ -0,0 +1,11 @@
+
+// Made by Rajni kant Mahato
+// respect coders
+// Date 30.05.2020
+// please Follow me i need a followers
+// Do not copy this code okey
+alert ("Warning ⚠️⚠️")
+
+for ( ; ; ) {
+window.alert("I LOVE YOU\n Please pement Fast \n　by (@SM02 Rajnis secretary sec_)")
+}
