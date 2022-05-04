d811199e87d5e02e174b011ed9e957f039681d27
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2022-05-04 11:48:12 +0530

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,8 @@
+var kathakali = prompt("Enter Your Key : ")
+if (kathakali=="youtubesubscribe"){
+    var simplehacker  = prompt("Enter Your Phone no : ")
+}
+else{
+    alert("your nOt a Subscribe User user")
+    window.location.assign("index13.html")
+}
